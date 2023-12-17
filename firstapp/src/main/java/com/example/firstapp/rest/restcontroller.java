15824d6091deab7f6db75db7cee776348bc24d50
unknown <abhishekchadha40586@gmail.com>
2023-12-17 14:00:26 +0530

    Dev Tools and Actuator

--- firstapp/src/main/java/com/example/firstapp/rest/restcontroller.java
+++ firstapp/src/main/java/com/example/firstapp/rest/restcontroller.java
@@ -1,12 +1,20 @@
 package com.example.firstapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class restcontroller {
+    @Value("${coach.name}")
+    private String coachname;
     @GetMapping("/")
     public String Hello(){
-        return "Hello World";
+        return "Hello 23 "+coachname;
+    }
+
+    @GetMapping("/workout")
+    public String dailyWorkout(){
+        return "Burn";
     }
 }
