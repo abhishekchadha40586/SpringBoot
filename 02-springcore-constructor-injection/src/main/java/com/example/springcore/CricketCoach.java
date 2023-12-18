d26aac908ebab1fcc3fc9025c76f7edebbbcfd9c
unknown <abhishekchadha40586@gmail.com>
2023-12-19 00:49:07 +0530

    Constructor Injection

---
+++ 02-springcore-constructor-injection/src/main/java/com/example/springcore/CricketCoach.java
@@ -0,0 +1,13 @@
+package com.example.springcore;
+
+import org.springframework.stereotype.Component;
+
+// creates cricketcoach as a bean
+@Component
+public class CricketCoach implements Coach{
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowling for 20 mins!!!";
+    }
+}
